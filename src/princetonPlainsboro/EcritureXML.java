afaf4f4af36b150bf54160179e3eff82e486c4fc
Dulac Julia <dulac.julia@gmail.com>
2015-05-28 17:16:34 +0200

    Interface_V1

---
+++ src/princetonPlainsboro/EcritureXML.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package princetonPlainsboro;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Vector;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+/**
+ *
+ * @author Cyril
+ */
+public class EcritureXML {
+    
+}
